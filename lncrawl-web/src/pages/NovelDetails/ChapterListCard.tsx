6dc2829aea7a0303cd4c3b395f1000dd9e9ec3e8
Sudipto Chandra <dipu.sudipta@gmail.com>
2025-11-27 23:06:26 +0400

    Improve scheduler and job runner

--- lncrawl-web/src/pages/NovelDetails/ChapterListCard.tsx
+++ lncrawl-web/src/pages/NovelDetails/ChapterListCard.tsx
@@ -3,6 +3,8 @@ import { stringifyError } from '@/utils/errors';
 import { DownloadOutlined, RightCircleOutlined } from '@ant-design/icons';
 import { Button, Collapse, message } from 'antd';
 import axios from 'axios';
+import VirtualList from 'rc-virtual-list';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ChapterDetailsCard } from './ChapterDetailsCard';
 
@@ -12,6 +14,8 @@ export const ChapterListCard: React.FC<{
 }> = ({ chapters, history = {} }) => {
   const navigate = useNavigate();
   const [messageApi, contextHolder] = message.useMessage();
+
+  const [activeKey, setActiveKey] = useState<any>(null);
 
   const createChapterJob = async (id: string) => {
     try {
@@ -24,56 +28,77 @@ export const ChapterListCard: React.FC<{
     }
   };
 
+  const handleChange = (key: string | string[] | null) => {
+    if (Array.isArray(key)) {
+      setActiveKey(key[0] ?? null);
+    } else {
+      setActiveKey(key as string | null);
+    }
+  };
+
   return (
     <div style={{ marginTop: 10 }}>
       {contextHolder}
-      <Collapse
-        ghost
-        accordion
-        size="small"
-        items={chapters.map((chapter) => ({
-          key: chapter.id,
-          label: chapter.title,
-          children: <ChapterDetailsCard inner chapter={chapter} />,
-          styles: {
-            body: { padding: 0 },
-            header: {
-              opacity: history[chapter.id] ? 0.5 : 1,
-              textTransform: 'capitalize',
-            },
-          },
-          extra: chapter.is_available ? (
-            <Button
-              size="small"
-              shape="round"
-              style={{ width: 75 }}
-              icon={<RightCircleOutlined />}
-              onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                navigate(`/read/${chapter.id}`);
-              }}
-            >
-              Read
-            </Button>
-          ) : (
-            <Button
-              size="small"
-              shape="round"
-              type="primary"
-              style={{ width: 75 }}
-              icon={<DownloadOutlined />}
-              onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                createChapterJob(chapter.id);
-              }}
-            >
-              Get
-            </Button>
-          ),
-        }))}
-      />
+      <VirtualList data={chapters} height={400} itemKey="id" itemHeight={40}>
+        {(chapter) => (
+          <Collapse
+            ghost
+            accordion
+            size="small"
+            key={chapter.id}
+            activeKey={activeKey}
+            onChange={handleChange}
+            style={{
+              borderRadius: 0,
+              borderBottom: '1px solid  #222d2d',
+            }}
+            items={[
+              {
+                key: chapter.id,
+                label: chapter.title,
+                children: <ChapterDetailsCard inner chapter={chapter} />,
+                styles: {
+                  body: { padding: 0 },
+                  header: {
+                    opacity: history[chapter.id] ? 0.5 : 1,
+                    textTransform: 'capitalize',
+                  },
+                },
+                extra: chapter.is_available ? (
+                  <Button
+                    size="small"
+                    shape="round"
+                    style={{ width: 75 }}
+                    icon={<RightCircleOutlined />}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      e.preventDefault();
+                      navigate(`/read/${chapter.id}`);
+                    }}
+                  >
+                    Read
+                  </Button>
+                ) : (
+                  <Button
+                    size="small"
+                    shape="round"
+                    type="primary"
+                    style={{ width: 75 }}
+                    icon={<DownloadOutlined />}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      e.preventDefault();
+                      createChapterJob(chapter.id);
+                    }}
+                  >
+                    Get
+                  </Button>
+                ),
+              },
+            ]}
+          />
+        )}
+      </VirtualList>
     </div>
   );
 };
